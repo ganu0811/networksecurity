e4e99e255062de2b03cf78296fb715c7befbf117
ganu0811 <ganufindsu@gmail.com>
2025-01-07 12:08:45 +0000

    Data Validation Completed

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from network_security.components.data_ingestion import DataIngestion
+from network_security.components.data_validation import DataValidation
 from network_security.exception.exception import NetworkSecurityException
 from network_security.logging.logger import logging
-from network_security.entity.config_entity import DataIngestionConfig
+from network_security.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from network_security.entity.config_entity import TrainingPipelineConfig
 import sys
 
@@ -15,7 +16,17 @@ if __name__=="__main__":
         logging.info("Initiate the Data ingestion")
         
         dataingestionartifact= data_ingestion.initiate_data_ingestion()
+        logging.info("Data Ingestion is completed")
         print(dataingestionartifact)
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
+        data_validation = DataValidation(dataingestionartifact, data_validation_config)
+        logging.info("Initaite the data validation")
+        data_validation_artifact= data_validation.initiate_data_validation()
+        logging.info('Data Validation Completed')
+        print(data_validation_artifact)
+        
+        
+        
     
     
     except Exception as e:
---
+++ network_security/utils/main_utils/utils.py
@@ -0,0 +1,29 @@
+import yaml
+import os, sys
+# import dill
+import pickle
+from network_security.exception.exception import NetworkSecurityException
+from network_security.logging.logger import logging
+import numpy as np
+
+
+def read_yaml_file(file_path:str)-> dict:
+    try:
+        with open(file_path, "rb") as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys) from e
+
+
+def write_yaml_file(file_path:str, content:object, replace:bool = False) -> None:
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        os.makedirs(os.path.dirname(file_path), exist_ok = True)
+        with open(file_path, "w") as file:
+            yaml.dump(content, file)
+    
+    except Exception as e:
+        raise NetworkSecurityException(e,sys) 
+    
